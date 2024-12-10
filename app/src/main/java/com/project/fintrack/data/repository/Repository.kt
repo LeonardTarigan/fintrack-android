792432a66c2f9bd5a47c604038cd80b38ec07b0d
Azarya Aria Alfathan <azaryaaria@gmail.com>
2024-12-10 18:49:04 +0700

    Integrasi dengan room database

--- app/src/main/java/com/project/fintrack/data/repository/Repository.kt
+++ app/src/main/java/com/project/fintrack/data/repository/Repository.kt
@@ -1,28 +1,11 @@
 package com.project.fintrack.data.repository
 
-import android.content.Context
-import com.project.fintrack.data.dao.TransactionDao
 import com.project.fintrack.data.db.LocalDatabase
-import com.project.fintrack.data.models.TransactionEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
-class Repository(context: Context) {
-    private val transactionDao: TransactionDao = LocalDatabase.getDatabase(context).transactionDao()
 
-    suspend fun getAllTransactions(): List<TransactionEntity> = withContext(Dispatchers.IO) {
-        transactionDao.getAll()
-    }
+class Repository(private val db: LocalDatabase) {
 
-    suspend fun createTransaction(transaction: TransactionEntity) = withContext(Dispatchers.IO) {
-        transactionDao.insert(transaction)
-    }
+    suspend fun getAllTransactions() = db.transactionDao().getAll()
 
-    suspend fun updateTransaction(transaction: TransactionEntity) = withContext(Dispatchers.IO) {
-        transactionDao.update(transaction)
-    }
 
-    suspend fun deleteTransaction(transaction: TransactionEntity) = withContext(Dispatchers.IO) {
-        transactionDao.delete(transaction)
-    }
 }
